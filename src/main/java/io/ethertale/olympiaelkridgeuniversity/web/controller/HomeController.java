394b053dce38d2a03369900553f1099669fc4040
Galin Yanakov <galin@ivel.in>
2025-06-30 21:28:11 +0300

    Main page with fragments and simple controller

---
+++ src/main/java/io/ethertale/olympiaelkridgeuniversity/web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package io.ethertale.olympiaelkridgeuniversity.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView("main");
+        return mav;
+    }
+}
