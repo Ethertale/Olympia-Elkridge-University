9b73fbbae106fa0d773436962041a050cb6b2964
Galin Yanakov <galin@ivel.in>
2025-07-01 23:01:34 +0300

    Added the home controller

--- src/main/java/io/ethertale/olympiaelkridgeuniversity/web/controller/HomeController.java
+++ src/main/java/io/ethertale/olympiaelkridgeuniversity/web/controller/HomeController.java
@@ -1,5 +1,7 @@
 package io.ethertale.olympiaelkridgeuniversity.web.controller;
 
+import io.ethertale.olympiaelkridgeuniversity.schools.service.SchoolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,17 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class HomeController {
 
+    private final SchoolService schoolService;
+
+    @Autowired
+    public HomeController(SchoolService schoolService) {
+        this.schoolService = schoolService;
+    }
+
     @GetMapping
     public ModelAndView home() {
         ModelAndView mav = new ModelAndView("main");
+        mav.addObject("LawBranches", schoolService.getAllLawBranches());
         return mav;
     }
 }
